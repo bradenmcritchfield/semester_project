bdec88b3cbec701448d520912351a15b1a75a5ec
Braden Critchfield <bradenmcritchfield@gmail.com>
2023-12-16 20:45:18 -0700

    more dashboard

--- DataProject/Dashboard.py
+++ DataProject/Dashboard.py
@@ -8,6 +8,7 @@ st.title("Board Games Bonanza")
 
 st.sidebar.info("This app is maintained by Braden Critchfield. All data comes from Board Games Geek and was pulled November 18, 2023")
 
+tab1, tab2, tab3 = st.tabs(["Histograms", "Violin Plots", "Scatterplots"])
 bg = pd.read_csv("DataProject/boardgamesdata.csv")
 bg["GroupSize"] = pd.Categorical(bg["GroupSize"], categories = ["Individual", "Small", "Large", "Massive"], ordered = True)
 bg["Time Category"] = pd.Categorical(bg["Time Category"], categories = ["Quick", "Short", "Moderate", "Long", "Very Long", "Marathon"], ordered = True)
@@ -20,10 +21,9 @@ options = ['Year Published', 'Min Players', 'Max Players', 'Playing Time',
        'Average USD Price', 'Age (Years)', 'Time Category', 'AgeRating',
        'GroupSize']
 
-selected_variable = st.selectbox('Select a variable', options) #First field is prompt, second field is options
-title = "Histogram of " + selected_variable
-plot = sns.histplot(data = bg, x = selected_variable).set_title(title)
-
-
-st.pyplot(plot.get_figure())
+with tab1:
+       selected_variable = st.selectbox('Select a variable', options) #First field is prompt, second field is options
+       title = "Histogram of " + selected_variable
+       plot = sns.histplot(data = bg, x = selected_variable).set_title(title)
+       st.pyplot(plot.get_figure())
 
