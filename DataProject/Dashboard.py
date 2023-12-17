c4288dc23753e2a5c9513b00f2f1c0c092b304ad
Braden Critchfield <bradenmcritchfield@gmail.com>
2023-12-16 23:48:21 -0700

    test

--- DataProject/Dashboard.py
+++ DataProject/Dashboard.py
@@ -52,7 +52,7 @@ with tab1:
 with tab3:
        select_variable2 = st.selectbox("Choose Variable", options3)
        title = select_variable2 + " by Year Published"
-       bg_agg1 = {"Year Published" : bg_agg.index(), select_variable2: bg_agg[select_variable2]}
-       df = pd.DataFrame(bg_agg1)
+       bg_agg1 = {select_variable2: bg_agg[select_variable2]}
+       df = pd.DataFrame(bg_agg1).reset_index()
        plot2 = sns.lineplot(data = df, x = "Year Published", y = select_variable2).set_title(title)
        st.pyplot(plot2.get_figure())
