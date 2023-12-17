2f6795e20ea2a6f11ad0479a93741c033d3571ca
Braden Critchfield <bradenmcritchfield@gmail.com>
2023-12-16 21:03:54 -0700

    remove outliers option

--- DataProject/Dashboard.py
+++ DataProject/Dashboard.py
@@ -14,6 +14,7 @@ bg["GroupSize"] = pd.Categorical(bg["GroupSize"], categories = ["Individual", "S
 bg["Time Category"] = pd.Categorical(bg["Time Category"], categories = ["Quick", "Short", "Moderate", "Long", "Very Long", "Marathon"], ordered = True)
 bg["AgeRating"] = pd.Categorical(bg["AgeRating"], categories = ["Young", "PreTeen", "Teen", "Adult", "Any"], ordered = True)
 
+bg_o = bg.drop(index = [49, 131, 164, 473, 823, 441, 690, 816, 916, 949])
 
 options = ['Year Published', 'Min Players', 'Max Players', 'Playing Time',
        'Age Minimum', 'Number of Accessories', 'Number of Ratings',
@@ -24,7 +25,10 @@ options = ['Year Published', 'Min Players', 'Max Players', 'Playing Time',
 with tab1:
        selected_variable = st.selectbox('Select a variable', options) #First field is prompt, second field is options
        outlier_switch = st.checkbox("Remove Outliers", value = False)
+       if(outlier_switch == False):
+              bg1 = bg
+       else: bg1 = bg_o
        title = "Histogram of " + selected_variable
-       plot = sns.histplot(data = bg, x = selected_variable).set_title(title)
+       plot = sns.histplot(data = bg1, x = selected_variable).set_title(title)
        st.pyplot(plot.get_figure())
 
