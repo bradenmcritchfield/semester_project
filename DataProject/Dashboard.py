80bca02882423ca2a227e19f5e7e4d05d9adef4e
Braden Critchfield <bradenmcritchfield@gmail.com>
2023-12-18 11:43:57 -0700

    add urls

--- DataProject/Dashboard.py
+++ DataProject/Dashboard.py
@@ -9,6 +9,14 @@ st.title("Board Games Bonanza")
 sidebar1 = st.sidebar
 sidebar1.info("This app is maintained by Braden Critchfield. All data comes from Board Games Geek and was accessed November 17, 2023.")
 sidebar1.image("DataProject/BGG.webp")
+url1 = "https://bradenmcritchfield.github.io/2023/12/12/Board-Game-Bonanza-Data-Collection.html"
+url2 = "https://bradenmcritchfield.github.io/2023/12/13/Board-Game-Bonanza-EDA.html"
+url3 = "https://github.com/bradenmcritchfield/semester_project/tree/main/DataProject"
+sidebar1.write("For more information, visit [my data collection blog post.](%s)" % url1)
+sidebar1.write("You can also visit [my data exploration blog post.](%s)" % url2)
+sidebar1.write("For relevant code, visit [my github repository.](%s)" % url3)
+
+
 #sidebar1.info("Visit bradenmcritchfield.github.io for blog posts about this data.")
 
 tab1, tab2, tab3 = st.tabs(["Histograms", "Violin Plots", "Over Time"])
